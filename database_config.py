efd18020fd8020d9c2ef9beecdead50e6cf34969
Nanmk9977 <nmk25513@gmail.com>
2025-06-17 09:20:18 +0530

    database_config.py

--- database_config.py
+++ database_config.py
@@ -1,9 +1,9 @@
-from flask_mysqldb import MySQL
+import psycopg2
 
-def init_mysql(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_PORT'] = 3306
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'Nandu_mk@07'
-    app.config['MYSQL_DB'] = 'lung_health'
-    return MySQL(app)
+def get_pg_connection():
+    return psycopg2.connect(
+        host="dpg-d18e21ruibrs73bqcc7g-a",
+        dbname="lungdata",
+        user="lungdata_user",
+        password="cSTm86aPOICCcnolOFWixkVkTd77vVk6"
+    )
